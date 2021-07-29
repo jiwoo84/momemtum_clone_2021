04e000bb829a544f9b9d5cb8784a48eb544027b1
Jiwoo <wldn0804@gmail.com>
2021-07-29 15:23:20 +0900

    #3.4

--- practice.js
+++ practice.js
@@ -1,1 +1,12 @@
-document.title = "hi";
+const title = document.querySelector("div h1");
+
+function titleMouseEnter() {
+  title.innerText = "Mouse is comming!";
+}
+
+function titleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("mouseenter", titleMouseEnter);
+title.addEventListener("mouseleave", titleMouseLeave);
