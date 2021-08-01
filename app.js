6cf7edfff241868bce5101913a682bb58ee1a409
Jiwoo <wldn0804@gmail.com>
2021-08-01 16:19:30 +0900

    #4.4

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginForm.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `hello ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
