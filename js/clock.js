6bf0dd4d91fa8176ad0fdbef3daf44960ff018eb
Jiwoo <wldn0804@gmail.com>
2021-08-05 12:15:50 +0900

    #5.0

---
+++ js/clock.js
@@ -0,0 +1,7 @@
+const clock = document.querySelector("#clock");
+
+function sayHello() {
+  console.log("Hello");
+}
+
+setInterval(sayHello, 5000);
--- js/greeting.js
+++ js/greeting.js
@@ -1,22 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function paintGreeting() {
-  const username = localStorage.getItem(USERNAME_KEY);
-  greeting.innerText = `hello ${USERNAME_KEY}`;
+function paintGreeting(paintUsername) {
+  greeting.innerText = `hello ${paintUsername}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY, loginInput.value);
-  paintGreeting();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -25,5 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreeting();
+  paintGreeting(savedUsername);
 }
