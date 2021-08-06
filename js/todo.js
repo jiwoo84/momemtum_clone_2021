bbd7818ce4995b5cf9dfd779486a336aa3cb131e
Jiwoo <wldn0804@gmail.com>
2021-08-06 18:10:27 +0900

    #7.3

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+//삭제 버튼 fnc
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 
+// todo 리스트 만드는 fnc
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
@@ -19,11 +27,14 @@ function paintToDo(newTodo) {
   toDoList.appendChild(li);
 }
 
+// event fnc
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
