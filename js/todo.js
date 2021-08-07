2cdbed6e39f9a573ce0a6fa35bae8b848bb2a703
Jiwoo <wldn0804@gmail.com>
2021-08-07 23:16:16 +0900

    #7.8

--- js/todo.js
+++ js/todo.js
@@ -15,6 +15,8 @@ function saveToDos() {
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 // todo 리스트 만드는 fnc
