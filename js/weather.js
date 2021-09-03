f1189f4893a7d6c9186c0e052004e27e694ecaa2
Jiwoo <wldn0804@gmail.com>
2021-09-03 15:22:52 +0900

    api 수정

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${9fa4320ce0f2af5804626dcb41e2a97e}&units=metric`;
 
   fetch(url)
     .then((response) => response.json())
